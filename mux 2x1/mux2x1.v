ba03a4054e8d60621eeb17d430f0f60e0d2be339
bharathkumar419 <bharathkumar63099@gmail.com>
2025-11-05 13:52:25 +0530

    Update mux2x1.v

--- mux 2x1/mux2x1.v
+++ mux 2x1/mux2x1.v
@@ -1,1 +1,15 @@
+module mux_2x1(
+    input a, b, sel,
+    output y
+);
+    reg y_reg;
+    always @(*) begin
+        case(sel)
+            1'b0: y_reg = a;
+            1'b1: y_reg = b;
+            default: y_reg = a;
+        endcase
+    end
+    assign y = y_reg;
+endmodule
 
