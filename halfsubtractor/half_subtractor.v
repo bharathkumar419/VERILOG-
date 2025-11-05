c8438b8d0246aa2fa7feeeae9622bc3ffb0f3726
bharathkumar419 <bharathkumar63099@gmail.com>
2025-11-05 13:20:53 +0530

    Update half_subtractor.v

--- halfsubtractor/half_subtractor.v
+++ halfsubtractor/half_subtractor.v
@@ -1,1 +1,8 @@
 
+module half_subtractor(
+    input a, b,
+    output diff, bout
+);
+    assign diff = a ^ b;          // XOR gate for difference
+    assign bout = (~a) & b;    // AND gate with inverted A for borrow
+endmodule
